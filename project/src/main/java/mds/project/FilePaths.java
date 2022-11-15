b2acdafe74afde975e5e1829018e6807650b19d8
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2022-11-15 19:38:59 +0100

    project

---
+++ project/src/main/java/mds/project/FilePaths.java
@@ -0,0 +1,8 @@
+package mds.project;
+
+import java.io.File;
+
+public class FilePaths {
+    public final static String VIDEO_FROM_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    public final static String DASH_DIRECTORY = "D:\\MDS\\files\\project\\streams\\";
+}
